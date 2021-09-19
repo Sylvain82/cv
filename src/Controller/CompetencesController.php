2bac2450960c7af3b1f452dd4a67acd8393dcd87
Sylvain82 <76108593+Sylvain82@users.noreply.github.com>
2021-09-19 20:15:37 +0200

    lien de telechargement

---
+++ src/Controller/CompetencesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CompetencesController extends AbstractController
+{
+    #[Route('/competences', name: 'competences')]
+    public function index(): Response
+    {
+        return $this->render('competences/index.html.twig');
+    }
+}
